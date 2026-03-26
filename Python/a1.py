e668c0e56d01a292ffa6d389e6698dd0f49da4ec
Sitesh Mishra <siteshmishra23@gmail.com>
2026-03-26 21:18:30 +0530

    Add files via upload

---
+++ Python/a1.py
@@ -0,0 +1,57 @@
+#1
+# name=input("Enter your name:")
+# age=int(input("Enter your age:"))
+# print(f"Hello {name}, You are {age} years old!")
+
+#2
+# a=int(input("Enter first number:"))
+# b=int(input("Enter second number:"))
+# print(f"product is {a*b} \n sum is{a+b} \n quotient is {a/b} \n remainder is {a%b} ")
+
+#3
+# i1=int(input("Enter first integer:"))
+# i2=int(input("Enter second integer:"))
+# f1=int(input("Enter first float:"))
+# avg= (float(i1)+float(i2)+f1)/3
+# print("Avg is:",avg)
+
+#4
+# num="45"
+# print(int(num),type(num))
+# print(float(num),type(num))
+# print(str(num),type(num))
+
+# 5
+# x=10+3*2**2
+# print(x)
+
+# 6
+# a=int(input("Enter first integer:"))
+# b=int(input("Enter second integer:"))
+
+# print(f"Before swap a is {a} b is {b}")
+# temp=0
+# temp=a
+# a=b
+# b=temp
+# print(f"after swap a is{a} b is {b}")
+
+# 7
+# c=input("Enter temperature in celsius:")
+# c=float(c)
+# print(f"temperature in fahrenhite is { (c*(9/5)) + 32}")
+
+# 8
+# r=int(input("Enter radius:"))
+# print(f"area in  { 3.14 * r**2}")
+
+# 9
+# principal=float(input("Enter principal amount:"))
+# rate=float(input("Enter rate:"))
+# time=float(input("Enter time:"))
+# print(f"simple interest is{(principal*rate*time)/100}")
+
+# 10
+
+# num=float(input("Enter A DECIMAL digit:"))
+# print(f"integer part is {int(num)} float part is {round(num-int(num),2)}")
